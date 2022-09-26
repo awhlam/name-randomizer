0e3be20cbbf4e6b3689b0749e1d78b03b181a897
Andrew Lam <alam1324@gmail.com>
2022-09-25 22:12:59 -0700

    Create directories, use relative paths, generate log file

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from random import shuffle
-from os import path, makedirs, getcwd
+from os import path, makedirs, mkdir
 from glob import glob
 from shutil import copy2, rmtree
+from datetime import datetime
 
-WORKING_DIR = getcwd()
-CLEAR_OUTPUT = True
-
-output_dir = path.join(WORKING_DIR, 'output')
-input_dir = path.join(WORKING_DIR, 'input')
-glob_dir = input_dir + "/**/*.*"
+DIR_NAMES = ["input", "output", "logs"]
+CLEAR_OUTPUT_DIR = True
+LOG_FILENAME = f"logs/log-{datetime.now()}.txt".replace(":", "")
+GLOB_DIR = "input/**/*.*"
 
 if __name__ == "__main__":
-    if CLEAR_OUTPUT:
-        rmtree(output_dir)
-        makedirs(output_dir)
+    for dir in DIR_NAMES:
+        if not path.exists(dir):
+            mkdir(dir)
 
-    files = glob(glob_dir, recursive=True)
+    if CLEAR_OUTPUT_DIR:
+        rmtree("output")
+        makedirs("output")
+
+    f = open(LOG_FILENAME, "w")
+    files = glob(GLOB_DIR, recursive=True)
     print(f"Number of files: {len(files)}")
     files = [file for file in files if 'exclude' not in file.lower()]
     print(f"Number of files after removing exclusions: {len(files)}")
+
     filenames = [str(i) for i in range(1, len(files) + 1)]
     shuffle(filenames)
 
@@ -26,8 +31,11 @@ if __name__ == "__main__":
         file_extension = path.splitext(source_path)[1]
         file_name = filenames.pop(-1)
 
-        output_name = file_name + file_extension
-        output_path = path.join(output_dir, output_name)
+        output_path = "output/" + file_name + file_extension
 
-        print(f"{source_path} => {output_path}")
+        log_message = f"{source_path} > {output_path}"
+        f.write(log_message + "\n")
+        print(log_message)
+
         copy2(source_path, output_path)
+    f.close()
