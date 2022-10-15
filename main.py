c4d45caec9162716d0767ec3f92f54a62f85164b
Andrew Lam <alam1324@gmail.com>
2022-10-14 18:28:57 -0700

    Refactor to multiple helper functions

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ LOG_FILENAME = f"logs/log-{datetime.now()}.txt".replace(":", "")
 GLOB_DIR = "input/**/*.*"
 
 def main():
+    setup_directories()
+    files, filenames = prepare_files()
+    copy_with_random_names(files, filenames)
+
+def setup_directories():
     for dir in DIR_NAMES:
         if not path.exists(dir):
             mkdir(dir)
@@ -18,31 +23,19 @@ def main():
         rmtree("output")
         makedirs("output")
 
-    f = open(LOG_FILENAME, "w")
+def prepare_files():
+    f = open(LOG_FILENAME, "a")
     files = glob(GLOB_DIR, recursive=True)
-    num_files = len(files)
-    print(f"Number of files: {num_files}")
+
+    # get file counts and exclude files
+    f.write(f"Number of files: {len(files)}\n")
     files = [file for file in files if 'exclude' not in file.lower()]
-    print(f"Number of files after removing exclusions: {len(files)}")
+    f.write(f"Number of files after removing exclusions: {len(files)}\n")
 
     filenames = [str(i) for i in range(1, len(files) + 1)]
     shuffle(filenames)
-
-    for source_path in files:
-        file_extension = path.splitext(source_path)[1]
-        file_name = filenames.pop(-1)
-
-        num_digits = count_digits(num_files)
-        file_name = file_name.zfill(num_digits)
-
-        output_path = "output/" + file_name + file_extension
-
-        log_message = f"{source_path} > {output_path}"
-        f.write(log_message + "\n")
-        print(log_message)
-
-        #copy2(source_path, output_path)
     f.close()
+    return files, filenames
 
 def count_digits(number):
     count = 0
@@ -50,6 +43,22 @@ def count_digits(number):
         number = number // 10
         count += 1
     return count
+
+def copy_with_random_names(files, filenames):
+    f = open(LOG_FILENAME, "a")
+    for source_path in files:
+        file_extension = path.splitext(source_path)[1]
+        file_name = filenames.pop(-1)
+
+        # pad filenames with leading zeros
+        num_digits = count_digits(len(files))
+        file_name = file_name.zfill(num_digits)
+
+        # move files and write to log
+        output_path = "output/" + file_name + file_extension
+        f.write(f"{source_path} > {output_path}\n")
+        copy2(source_path, output_path)
+    f.close()
 
 if __name__ == "__main__":
     main()
